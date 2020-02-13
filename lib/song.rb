bfa7002c9424a62d60050e0f292ef3851538382f
James Rogers <jarogers095@gmail.com>
2020-02-13 02:41:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    filename_pieces = filename.split(/ - |\.mp3/)
+    filename_pieces = filename.split(/ - /)
     name_of_song = filename_pieces[0]
     name_of_artist = filename_pieces[1]
     song_object = Song.new(name_of_song)
