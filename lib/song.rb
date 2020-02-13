313bc98dec1eb05e58f72bb831383a30e38c41c2
James Rogers <jarogers095@gmail.com>
2020-02-13 02:59:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ class Song
     name_of_artist = filename_pieces[0]
     name_of_song = filename_pieces[1]
     song_object = Song.new(name_of_song)
-    song_object.artist = Artist.new(name_of_artist)
+    song_object.artist = Artist.find_or_create_by_name(name_of_artist)
     return song_object
   end
 end
